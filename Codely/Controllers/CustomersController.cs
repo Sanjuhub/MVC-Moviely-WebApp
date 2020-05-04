ea70d3cd2ca246e93d250b0eac406a4b970a8998
sanjay kumar <sanjay14324@gmail.com>
2020-05-05 01:46:59 +0530

    Added Client side validation and CSRF token validation

--- Codely/Controllers/CustomersController.cs
+++ Codely/Controllers/CustomersController.cs
@@ -33,6 +33,7 @@ namespace Codely.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)  
         {
             if (!ModelState.IsValid)
