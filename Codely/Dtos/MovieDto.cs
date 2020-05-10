6389f75c57d9f40dab6b94f7d9c1601cef8c2de9
sanjay kumar <sanjay14324@gmail.com>
2020-05-11 01:42:25 +0530

    Added Datatables

--- Codely/Dtos/MovieDto.cs
+++ Codely/Dtos/MovieDto.cs
@@ -13,7 +13,7 @@ namespace Codely.Dtos
         [StringLength(255)]
         public string Name { get; set; }
 
-        
+
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
